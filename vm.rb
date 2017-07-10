f1c167b155edf97ceb067f973e912ab7de82ea92
monstruooo <monstruooo@gmail.com>
2017-07-10 15:02:50 +0300

    Create vm.rb

---
+++ vm.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'rbvmomi'
+require 'pp'
+#require 'alchemist'
+
+
+#print "User: "
+#user = gets
+
+#print "Password: "
+#require 'io/console'
+#pass = STDIN.noecho(&:gets).chomp
+#puts
+
+creds = [ 'vmcenter', 'vmuser' , 'vmpass' ]
+creds.each do |cred|
+        unless  ENV[cred] =~ /[a-zA-Z]/
+                puts "Define \"#{cred}\" in your shell"
+                #cred == 'vmpass' ? (puts "\tExample: read -r -s #{cred} ; export #{cred}") : (puts "\tExample: export #{cred}=<#{cred}>")
+                cred == 'vmpass' ?
+                        (puts "\tExample: read -r -s #{cred} ; export #{cred}")
+                        :
+                        (print " \t Example: export #{cred}=" ; puts "<#{cred.sub(/^vm/,'')}> ")
+                exit
+        end
+end
+
+puts "Connecting..."
+vim = RbVmomi::VIM.connect :host => hyper, :user => user, :password => pass, :insecure => true
+
+puts "getting time"
+
+#
+# get current time
+#
+puts "Time on #{hyper}: " + vim.serviceInstance.CurrentTime.to_s
+
